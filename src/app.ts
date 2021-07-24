6cb560f853145120a1b3b1fe53198ec7f68cb4b5
StevenZoo <51144330+StevenZoo@users.noreply.github.com>
2021-07-24 15:30:13 -0700

    Add default error handling (#18)
    * Add default error handling

--- src/app.ts
+++ src/app.ts
@@ -4,8 +4,16 @@ let api = require("./routes/api");
 const app = express();
 const PORT = 3000;
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use("/api", api);
+
+app.use((err: any, req: any, res: any, next: any) => {
+  console.error(err);
+  res.status(err.status || 500);
+  res.send({
+    message: "Internal server error :(",
+  });
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);

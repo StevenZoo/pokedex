50dc665f48a5a03ac67b138ac553a5276d9fa32d
Steven Xu <51144330+StevenZoo@users.noreply.github.com>
2021-07-24 12:32:02 -0700

    Split webpack files for dev and production

---
+++ webpack.common.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: "./src/web/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [
+              [
+                "@babel/preset-env",
+                {
+                  corejs: 3,
+                  useBuiltIns: "usage",
+                },
+              ],
+            ],
+          },
+        },
+      },
+    ],
+  },
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: "./src/web/index.html", to: "index.html" }],
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".ts"],
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "public"),
+  },
+};
